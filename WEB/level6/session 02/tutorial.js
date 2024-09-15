6f67ee28c0b97c4fb93d8d8eb1d455fb7df96fa9
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2024-09-15 17:23:52 +0300

    Second commit

---
+++ WEB/level6/session 02/tutorial.js
@@ -0,0 +1,22 @@
+/*
+Template literals: provide a more flexible way to work with strings.
+    They are enclosed by backticks (``) instead of single or double quotes
+    to use variable inside the `` => ${}
+
+*/
+
+// var num1 = 9;
+// var num2 = 5;
+
+// let result = num1 + num2;
+
+// console.log(`The Addition of ${num1} and ${num2} is ${result} `);
+
+// console.log('Hello All and Welcome Mohamed');
+// console.log('Hello All and \n Welcome Mohamed');
+
+// console.log(`Hello All and
+//             Welcome Mohamed`);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
