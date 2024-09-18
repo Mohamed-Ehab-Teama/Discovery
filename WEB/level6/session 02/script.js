44593c428e1ec916c14f55463481035dd45c1584
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2024-09-19 00:01:58 +0300

    19-9-2024

---
+++ WEB/level6/session 02/script.js
@@ -0,0 +1,69 @@
+
+// var x = 50
+// var y = 26
+
+// var result = x + y
+
+// console.log("the sum of x and y is result")
+
+// console.log(`the sum of ${x} and ${y} is ${result}`);
+
+// console.log("I'm Mohamed Welcome ")
+// console.log("I'm Mohamed \n Welcome ")
+// console.log(`Hello, I'm Mohamed
+//         Welcome `)
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// var h1 = document.getElementById('head1')
+
+// h1.innerText = 'Hello World'
+
+// console.log(h1)
+
+
+// var pa = document.getElementById('para')
+
+// pa.innerText = 'We are in Web session'
+// pa.innerHTML = "Heelo There"
+
+// console.log(pa)
+
+// var div = document.getElementById('divv')
+// div.innerText = "<h1>We are in div</h1>"
+// div.innerHTML = "<h1>We are in div</h1>";
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// var first_h1 = document.querySelector('h1')
+
+// var div1 = document.querySelector('div')
+
+// console.log(first_h1)
+// console.log(div1)
+
+
+// var all = document.querySelectorAll('h1')
+
+// console.log(all)
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// var h1 = document.getElementById('head1')
+
+// h1.style.color = 'red'
+// h1.style.backgroundColor = "black"
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// var div = document.getElementById('di').classList
+
+// div.add('here', 'We-go')
+// div.remove('one','two','three')
+
+// console.log(div)
+// console.log(div.length)
+
+////////////////////////////////////////////////////////////////////////////////////
--- WEB/level6/session 02/tutorial.js
+++ WEB/level6/session 02/tutorial.js
@@ -18,5 +18,61 @@ Template literals: provide a more flexible way to work with strings.
 // console.log(`Hello All and
 //             Welcome Mohamed`);
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+The Document Object Model (DOM) is a programming interface for web documents, representing the structure of a document as a tree of objects.
+    It allows JavaScript to access and manipulate HTML and CSS, dynamically updating content and style.
+    returns null if not found.
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Get Element by ID:
+<div id="myDiv">Hello, world!</div>;
+myHeading = document.getElementById("myDiv");
+console.log(myDiv.innerText);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The querySelector() method in JavaScript is used to select the first element that matches a specified CSS selector or group of selectors within the document. If no matches are found, it returns null.
 
+const firstParagraph = document.querySelector("p"); // Selects the first <p> element in the document
+
+const myElement = document.querySelector("#myElement"); // Selects the element with the ID 'myElement'
+
+const x = document.querySelector(".example"); // Get the first element with class="example":
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The querySelectorAll() method returns all elements that matches a CSS selector(s).
+// The querySelectorAll() method throws a SYNTAX_ERR exception if the selector(s) is invalid
+
+document.querySelectorAll(".example"); // Select all elements with class="example":
+
+const nodeList = document.querySelectorAll("p");
+nodeList[0].style.backgroundColor = "red";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Inner Text : A property used to set or get the text content of an HTML element, excluding any HTML tags.
+// Inner HTML : A property used to set or get the HTML content within an element, allowing for the insertion of HTML tags and content.
+let html = document.getElementById("myP").innerHTML;
+document.getElementById("demo").innerHTML = "I have changed!";
+
+let text = document.getElementById("myP").innerText;
+document.getElementById("demo").innerText = text;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Style : An object representing the inline styles of an HTML element, allowing for the direct manipulation of CSS properties via JavaScript.
+element.style.backgroundColor = "red";
+document.getElementById("myH1").style.color = "red";
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Class List : A property that provides methods to manipulate the class attribute of an element, including adding, removing, and toggling classes.
+// It is useful for dynamically changing the appearance and behavior of elements.
+
+const list = document.getElementById("myDIV").classList;
+document.getElementById("demo").innerHTML = list;
+
+
+let numb = element.classList.length; // How many class names the element have:
+
+element.classList.add("myStyle", "anotherClass", "thirdClass");
+
+element.classList.remove("myStyle", "anotherClass", "thirdClass");
+
+let cl = element.classList.contains("myStyle"); // True or False
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
