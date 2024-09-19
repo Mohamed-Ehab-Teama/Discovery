c6ea06672c143fd765223fcd3ab0d445f73deb26
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-09-19 10:13:42 +0300

    Salma laptop

---
+++ WEB/level5/Session01/session1.js
@@ -0,0 +1,74 @@
+//this is java script code
+// console.log("Hello World")
+
+// Single Line Comment
+// console.log("Hello World")
+
+/* Multi-line comment
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+*/
+
+
+// Variables
+
+// var name = "Mohamed"
+// var name = "Ahmed"
+// name = "Hello"
+
+// let name2 = "Ali"
+// name2 = "Mahmoud"
+// name2 = "Omar"
+
+// const name3 = "Mona"
+
+// console.log(name)
+// console.log(name2)
+// console.log(name3)
+
+
+
+// Naming Rules
+// let first_name = "Mohamed"
+// let last_name = "Ehab"
+
+// camelCase   fisrtName
+// snakeCase   fisrt_name
+// PascalCase  FirstName
+
+
+
+
+
+// Data Types
+// x = 10          // number
+// x = 10.5        // number
+
+// s = "Ali"       // String
+
+// n = null        // NULL Value => There is no Value
+
+// n = NaN         // Not a number Value
+
+// t = true        // Boolean
+// f = false       // Boolean
+
+
+
+// Operators [+     -    *       /       = ]
+
+// console.log(5 + 10)
+// console.log(30 - 10)
+// console.log(30 * 10)
+// console.log(30 / 10)
+// let first_name = "Omar "
+// let mobileName = "Huawei"
+
+// console.log(first_name + mobileName + "Zeroo")
