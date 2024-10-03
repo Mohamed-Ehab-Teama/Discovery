28ff68abd3cff5f531f82a6671d61f73c6643e5b
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-10-03 14:42:05 +0300

    prepare for friday 4-10-2024 PHP

---
+++ PHP/Session02/session.php
@@ -0,0 +1,110 @@
+<?php
+/*
+Constants - Predefined Constants - Magic constants
+    to define constant:
+        1- define (name, value, case-sensitive)
+        2- const name = value
+
+                define()	                                                const keyword
+            Defines constants at runtime	                        Defines class constants at compile time
+            Can be defined anywhere in the script	                Can only be defined within classes or interfaces
+            Case-insensitive by default	                            Case-sensitive by default
+            Global scope	                                        Class scope
+
+    Pre-defined Constants:
+        PHP_VERSION     PHP_INT_MAX     PHP_INT_MIN     PHP_FLOAT_MAX       PHP_FLOAT_MIN
+    
+    Magic Constants:
+        __DIR__     __FILE__        __LINE__
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+Arithmetic Operators:
+    +   -   *   /   %   **
+
+Assignment Operators:
+    =   +=  -=  *=  /=  %/
+
+Comparison Operators:
+    ==  ===     !=  <>  !==     >   <   >=  <=          <=> (SpaceShip)
+
+Increment / Decrement Operators:
+    $x++    $x--    ++$x    --$x
+
+Logical Operators:
+    and     &&     or      ||      !
+
+*/
+
+
+
+
+
+
+
+// Conditional Statements:
+/*
+    if      if else         if elseif else
+        if(condition){
+            code
+        }
+
+        if(condition){
+            code
+        }else{
+            code
+        }
+
+        if(condition){
+            code
+        }elseif (condition) {
+            code
+        }else {
+            code
+        }
+
+    Short hand if:
+        if (condition) ? True : False ;
+*/
+
+
+
+
+
+/*
+    Switch:
+
+        switch (expression) {
+            case label1:
+                //code block
+                break;
+            case label2:
+                //code block;
+                break;
+            case label3:
+                //code block
+                break;
+            default:
+                //code block
+            }
+        
+        switch (expression) {
+            case label1:
+            case label2:
+            case label3:
+                //code block
+                break;
+            default:
+                //code block
+            }
+
+*/
---
+++ PHP/Session03/session.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Loops:
+*/
+
+
+/*
+Arrays:
+    define array:
+        1- $arr = [value, value, value]
+        2- $arr = array(value, value, value)
+
+    Indexed arrays:             $arr = [1,2,3,4,5,6]
+    Associative arrays:         $arr = ['key' => value, 'key' => value]
+
+*/
