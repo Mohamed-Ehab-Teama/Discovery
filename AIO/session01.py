cebf67ae1e75ab8f0ea781a75fdcbd556cdbb9a5
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2024-10-12 21:12:19 +0300

    Saturday 12/10/2024 at 21:12

--- AIO/session01.py
+++ AIO/session01.py
@@ -1,19 +1,19 @@
-# x = 10
-# if ( x > 10):
-#     print('greater than 10')
-# elif (x == 10):
-#     print('equal to 10')
-# else:
-#     print('less than')
+x = 10
+if ( x > 10):
+    print('greater than 10')
+elif (x == 10):
+    print('equal to 10')
+else:
+    print('less than')
 
-# name = 'ahmed'
-# name2 = 'ali'
+name = 'ahmed'
+name2 = 'ali'
 
-# print (name + ' ' + ' ###' + '-----'  + '  ' + name2)
+print (name + ' ' + ' ###' + '-----'  + '  ' + name2)
 
-# for (i) in range(1,12):
-#     str = ''
-#     for (j) in range(1,i):
-#         str = str + '*'
+for (i) in range(1,12):
+    str = ''
+    for (j) in range(1,i):
+        str = str + '*'
     
-#     print (str)
+    print (str)
