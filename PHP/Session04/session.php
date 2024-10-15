f26f04b9d3bc60933b43355719643eb2c2374c5d
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-10-15 16:26:10 +0300

    Tuseday 15/10/2024 at 16:26

--- PHP/Session04/session.php
+++ PHP/Session04/session.php
@@ -5,9 +5,14 @@ functions:
 built-in functions [Math, array, string]
     array functions :    array_push()    array_pull()    array_unshift()    array_shift()            count()     explode(separator, value)
     string functions :   strlen()    strrev()    trim()  implode(separator, value)
+    math functions :
 */
-
-
+/*
+search :
+    - String functions
+    - Array functions
+    - Math functions
+*/
 
 /*
 User defined functions:
@@ -18,6 +23,57 @@ User defined functions:
     function_name(arguments);
 */
 
+
+// function hello() {
+//     return "hello";
+// }
+// hello();
+
+// function add ($x , $y){
+//     echo $x + $y;
+// }
+// add(5,20);
+
+
+// function add3 ( $x , $y , $z ) {
+//     return $x + $y + $z;
+// }
+// echo add3(15,60,100);
+
+// function speak ($name = 'Unknown') {
+//     return "Hello $name";
+// }
+// echo speak("Ali");
+
+// $arr = [10,20,30,40];
+
+// array_push($arr , 10);
+// array_unshift($arr , 10);
+// array_shift($arr);
+// array_pop($arr);
+
+// $str = implode( "<hr>", $arr);
+// echo $str;
+
+// $s = "Mohamed Ehab Teama BFCAI IS";
+// $arr2 = explode ( "e", $s );
+
+// echo '<pre>';
+// print_r($arr2);
+// echo '</pre>';
+
+// $str = " ##Mohamed## ";
+// echo strrev($str);
+// echo "<hr>";
+// echo strlen($str);
+// echo "<hr>";
+// echo trim ($str, " #");
+
+
+// $arr = [20,200,50,60,748];
+// $x = max($arr);
+// echo min($arr);
+// echo min($arr);
 
 
 
@@ -31,4 +87,12 @@ Superglobals
     $_COOKIE
     $_SESSION
 */
+
+// echo "<pre>";
+// print_r( $GLOBALS);
+// print_r($_SERVER);   $_SERVER['REQUEST_METHOD'] == 'GET POST';
+// print_r($_REQUEST);
+// print_r($_GET);
+// print_r($_POST);
+// echo "</pre>";
 
