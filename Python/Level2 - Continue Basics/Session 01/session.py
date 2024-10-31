40712d7917f27a20c82799a7f5419ca8c54253a3
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-10-31 15:56:25 +0300

    Thrusday - 31/10/2024 - 15:56

---
+++ Python/Level2 - Continue Basics/Session 01/session.py
@@ -0,0 +1,56 @@
+# print ( 'hello ','Ali',' ' )
+# print ( " Hello 'Ali' " )
+# print ( ' Hello "Ali" ' )
+
+# name = "Ahmed"
+# print ( name[4] )
+
+# name = "Mohamed Ehab"
+
+# for i in name:
+#     print(i)
+
+# x = 'Ali'
+# y = "Mona"
+
+# str = ''' Hello, I am Mohamed
+#         Welcome to Python
+#         Hello Again
+#         Hello Again
+#         Hello Again
+#         '''
+
+# a = ''' Hello '''  
+      
+# a = """ Hello """  
+        
+# str1 = """
+#     Hello, I am Mohamed
+#         Welcome to Python
+#         Hello Again
+#         Hello Again
+#         Hello Again
+#         """
+
+# print (str1)
+
+# name = "moHAmed"
+# print ( name.capitalize() )
+# print ( name.upper() )
+# print ( name.lower() )
+
+# name = "   @..,,.,,.$  %  ___Mohamed    ..,,.,.,  "
+# print ( name )
+# print ( name.strip("@ . , $ % _") )
+
+# str = " Hello, and welcome, Hello, and Hi "
+# print ( str.count('el', 8, 18) )
+
+# he = 'Hello Mohamed Hello'
+# print ( he.startswith('Hello') )
+# print ( he.endswith('Hello') )
+
+# print ( he.find('Mohamed') )
+# print ( he.find('ibr') )
+# print ( he.index('Mohamed') )
+# print ( he.index('ibr') )
--- Python/Level5 Pygame/Session01/tutorial.py
+++ Python/Level5 Pygame/Session01/tutorial.py
@@ -4,4 +4,6 @@
 # That depends on the type of game you want to develop. If you are a beginner in game development, you can create a simple 2D game, such as flip-flop, and for that, you should choose Pygame. On the other hand, if you aim to develop advanced games, then you should go for Unity.
 
 # To install Pygame, open the command prompt and give the command as shown below:
-#     pip install pygame
+    # pip install pygame
+    # pip3 install pygame
+    
