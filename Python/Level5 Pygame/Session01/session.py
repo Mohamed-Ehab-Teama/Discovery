bed3c59dbe017d03e1dda789478d85a152e28441
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-11-04 17:21:01 +0200

    Monday - 4/11/2024 - 17:20

--- Python/Level5 Pygame/Session01/session.py
+++ Python/Level5 Pygame/Session01/session.py
@@ -2,17 +2,23 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode( (500,450) )
-pygame.display.set_caption(' Session 01 Introduction ')
+# Set Width and height of the game window
+screen = pygame.display.set_mode( (500,400) )
+# Set the Title of the game Window
+pygame.display.set_caption( ' My First Game ' )
 
+# Set Icon For the Game Window
 myIcon = pygame.image.load('Yamamoto .jpg')
-pygame.display.set_icon( myIcon )
+pygame.display.set_icon(myIcon)
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            
-    screen.fill( 'gold' )
+    
+
+    screen.fill( 'violet' )
+
 
     pygame.display.flip()
