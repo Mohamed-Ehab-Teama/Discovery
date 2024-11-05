ad4e6634c16eb4c67d44544b4d134f666eae588f
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-11-05 16:13:21 +0200

    Tuseday - 5/11/2024 - 16:13

---
+++ Python/Level2 - Continue Basics/Session 02/tutorial.py
@@ -0,0 +1,38 @@
+# Lists are used to store multiple items in a single variable
+# Lists are: Ordered, Changeable, Allow Duplicates
+# List items are ordered, changeable, and allow duplicate values
+# List items can be of any data type:
+    # list1 = ["abc", 34, True, 40, "male"]
+
+
+# To create a list:
+    # my_list = ["apple", "banana", "cherry"]
+    
+# Accessing list items:
+    # List items are indexed, the first item has index [0], the second item has index [1]
+    # print ( my_list[0] )
+    # print ( my_list[1] )
+    # print ( my_list[2] )
+    
+# Determining the length of a list:
+    # To determine how many items a list has, use the len() function:
+        # print( len(mty_list) )
+        
+# Getting a range of items from a list:
+    # print(thislist[0])        => Get the first item in the list
+    # print(thislist[1])        => Get the second item in the list
+    
+    # print(thislist[-1])        => Get the first last item in the list
+    # print(thislist[-2])        => Get the second last item in the list
+    
+    
+    # print(thislist[1:4])        => Get the items from position 1 to 4
+    
+# Note: In the range: The last element is not included
+        
+    # print(thislist[0:4])        => Get the items from position 0 to 4 [value in index 4 not returned]
+    # print(thislist[:4])        => Get the items from the start to position 4 [value in index 4 not returned]
+        
+    # print(thislist[2:])        => Get the items from position 2 to the end
+    
+    # print(thislist[-4:-1])        => Get the items from position -4 to -1 [value in index -1 not returned]
