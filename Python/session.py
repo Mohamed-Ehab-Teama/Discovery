054378a694ba0438d9f0a43c91d92d5a64cd0311
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-11-08 17:27:41 +0200

    Friday - 8/11/2024 - 17:27

---
+++ Python/session.py
@@ -0,0 +1,54 @@
+
+# x = 10
+
+# while x < 20 :
+#     print(x)
+#     x += 1
+    
+# x = x + 1
+# x += 1
+# x *= 1
+# x /= 10
+# x++ 
+
+# print ( 18 % 3 )
+
+# x = 0
+# while x <= 12:
+#     if x % 2 != 0:
+#         print(x, "ODD")
+#     x += 1
+
+# print( 2 ** 5)
+
+
+# x = 0
+
+# while x <= 10:
+#     if x == 5:
+#         x += 1
+#         continue
+#     print(x)
+#     x += 1
+
+# x = input('Enter Your Name \n')
+# print(x)
+
+# y = int( input('enter number \n') )
+# print( type(y) )
+
+# x = 0
+# num = int( input('Enter Number \n') )
+
+# while x < num:
+#     if x % 2 == 0:
+#         print(x)
+#     x += 1
+
+# start = int( input("Enter the beggining \n") )
+# end = int( input("Enter the last \n") )
+
+# while start <= end:
+#     if start % 5 == 0 :
+#         print(start)
+#     start += 1
