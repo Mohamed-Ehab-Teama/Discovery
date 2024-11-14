693a8df25dfd59f114fcf97e8cd3e9ac6d6c4f75
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-11-14 17:44:49 +0200

    Thrusday - 14/11/2024 - 17:44

---
+++ Marawan/Session 05/session.php
@@ -0,0 +1,97 @@
+<?php
+
+// $cars = [];
+// $cars = [10,20,30,40];
+// $cars = [10.7, 20.5, 30.1, 40.4];
+// $cars = [ "one", "two", "Three" ];
+// $cars = [ true, false, true ];
+// $cars = [ 1, 2.3, "Two", False ];
+
+// $arr0 = [];
+// $arr1 = array();
+
+// $arr2 = [10,20,30];
+// $arr = array("one", 200 , "Test" );
+
+// var_dump( $arr );
+
+// echo $arr[2];
+
+// echo $arr[0];
+
+// echo "<br>";
+
+// $arr = [ 10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 ];
+
+// $arr = [
+//     100 => " Ahmed " ,
+//     200 => " Ali " ,
+//     300 => 2000
+// ];
+
+// $arr = [
+//     "one" => "Ahmed",
+//     "two" => "Ali",
+//     "three" => "Marwan",
+// ];
+
+// echo $arr['two'];
+
+// $arr = [ 10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 ];
+
+// $arr = [
+//     10,
+//     20,
+//     30,
+//     [ "One", "Two", "Three" ],
+//     40,
+//     50,
+//     60,
+//     ["10", "Three"]
+// ];
+
+// echo $arr[3][2];
+
+// echo "<pre>";
+// print_r( $arr );
+// echo "</pre>";
+
+
+
+// $a = "Ahmed";
+// $A = "Ali";
+
+// $user_name = "Ahmed Ali";
+// $UserName = "Ahmed Ali";
+// $userName = "Ahmed Ali";
+
+// $x = $y = $s = 10;
+
+// $x = "web";
+// $$x = "Development";
+// $$$x = "Discovery";
+
+// echo $$$x;
+// echo $$$x;
+
+// echo "<br>";
+
+// echo $Development;
+
+// $a = "Ali";
+// $b = &$a;
+
+// $a = "Mona";
+
+// echo $a;
+// echo "<br>";
+// echo $b;
+
+
+
+define("USER_NAME", "Mohamed Ehba Teama");
+
+// echo USER_NAME;
+
+echo __LINE__;
+echo __line__;
