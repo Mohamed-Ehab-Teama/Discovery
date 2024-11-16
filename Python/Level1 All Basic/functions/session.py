64c18bd2be5170845f5a40e9b7f39ad35f76e4a2
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-11-16 17:50:53 +0200

    Saturday - 16/11/2024 - 17:50

---
+++ Python/Level1 All Basic/functions/session.py
@@ -0,0 +1,37 @@
+
+# def add():
+#     print ( 10 + 20 )
+# add()   
+# print (add())
+
+# def add ( x , y ):
+#     print ( x + y )
+# add(10,50)
+# add( 100, 50)
+
+
+
+# def add( x , y ):
+#     print(x + y)
+# add( y = 80, x = 40 )
+
+# li = [10,20,30,40,50]
+# print ( sum(li) )
+# print ( min(li) )
+# print ( max(li) )
+# print ( len(li) )
+
+# str = " Python is great, keep going "
+# print ( len(str) )
+# ---------------------------------------
+# def avg ( li ):
+#     print ( sum(li) / len(li) )
+# avg( [1,2,3,4] )
+# ---------------------------------------
+# def even_or_odd (num):
+#     if num % 2 == 0 :
+#         print("Even")
+#     else:
+#         print("odd")
+# even_or_odd( 10 )
+# ---------------------------------------
