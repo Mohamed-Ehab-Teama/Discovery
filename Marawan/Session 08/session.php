3faaa0bca618dd8d39ab93681b6f4435346dd1cc
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-12-05 17:00:20 +0200

    Thrusday - 5/12/2024 - 17:00

---
+++ Marawan/Session 08/session.php
@@ -0,0 +1,19 @@
+<?php
+
+// while ( condition ){
+//     code
+// }
+
+// $day = 0;
+
+// while ( $day <= 10 ){
+//     echo "$day <br>";
+//     $day++;
+// }
+
+// while ( $day <= 16 ){
+//     if ( $day % 2 != 0 ){
+//         echo $day . "<br>";
+//     }
+//     $day++;
+// }
