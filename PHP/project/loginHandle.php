e10b6d9c85bc6d75c1edee16640b8144b0876036
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-12-06 13:52:14 +0200

    Friday - 6/12/2024 - 13:52

--- PHP/project/loginHandle.php
+++ PHP/project/loginHandle.php
@@ -2,61 +2,49 @@
 
 require_once './connection.php';
 
-if ( $_SERVER['REQUEST_METHOD'] != 'POST' )
-{
+if ( $_SERVER['REQUEST_METHOD'] != 'POST' ){
     echo " Something Went Wrong ";
     die;
 }
 
 
-foreach ( $_POST as $key => $value )
-{
+
+foreach ( $_POST as $key => $value ){
     $$key = $value;
 }
-
 // $email
 // $pass
 
+
 if ( empty($email) or empty($pass) ){
-    echo " Fields cannot be Empty ";
+    echo "Fields cannot be empty";
     die;
 }
 
-if ( !filter_var( $email , FILTER_VALIDATE_EMAIL) ){
-    echo " Email is Not Valid ";
+if ( !filter_var($email, FILTER_VALIDATE_EMAIL) ){
+    echo " InValid Email ";
     die;
 }
 
-$email = filter_var( $email, FILTER_SANITIZE_EMAIL );
+$email = filter_var( $email, FILTER_SANITIZE_EMAIL);
 
 $pass = md5($pass);
 
 
 $sql = " SELECT * FROM users ";
-$res = mysqli_query( $connection, $sql );
+$result = mysqli_query($connection, $sql);
 
-if ( $res ){
-
-    while ( $user = mysqli_fetch_assoc( $res ) ){
-
-        // echo "<pre>";
-        // var_dump($user);
-        // echo "</pre>";
-
-        if ( $email == $user['email'] and  $pass == $user['password'] ){
-            header('location:index.php');
+if ( $result ){
+    while ( $row = mysqli_fetch_assoc($result) ){
+        
+        if ( $email == $row['email'] and $pass == $row['password'] ){
+            echo "Login Success";
             die;
-            // echo " Login Success ";
-            // die;
-        }
-        else{
-            echo " Login fail ";
-            die;
+        }else{
+            echo "Login Failed";
             // header('location:login.php');
-            // die;
+            die;
         }
 
     }
-    
-
 }
