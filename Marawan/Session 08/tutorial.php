85b1e7cc7e6caa659a41bab4d1e4574bc4a3c6da
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-12-15 15:42:53 +0200

    Sunday - 15/12/2024 - 15:42

--- Marawan/Session 08/tutorial.php
+++ Marawan/Session 08/tutorial.php
@@ -127,4 +127,4 @@ The require_once() statement is identical to require() except:
     PHP will check if the file has already been included, and if so, not include (require) it again.
 */
 
-$name = "Ali";
+// $name = "Ali";
---
+++ WEB/level5/Session01/script.js
@@ -0,0 +1,70 @@
+
+// const cars = [ "BMW" , "Chevorlet", "Marcedes", "Volvo" ];
+// const integ = [1,2,3,4,5];
+// const bo = [true, false, false, true];
+// const dou = [1.2 , 1.5 , 10.5];
+
+// const arr = ["Ali", 25 , 173.5, true, false, 5, 15];
+
+// console.log( arr )
+
+// console.log( arr[0] )
+// console.log( arr[2] )
+// console.log( arr[4] )
+// console.log( arr[3] )
+
+// console.log( arr.length ) // count values in an array
+
+// console.log( arr[ arr.length - 1 ] )    // get the last element
+// console.log( arr[ arr.length - 2 ] )    // get the pre-last element
+
+// console.log( '------------------------' )
+
+// const arr2 = ["Ali", 25 , 173.5, true, false, 5, 15];
+
+// arr2.push("Pre-Last Element")
+// arr2.push("Last Element")
+
+// console.log( arr2 )
+
+// arr2.pop()
+// arr2.pop()
+// arr2.pop()
+// arr2.pop()
+// arr2.push("End")
+
+// console.log( arr2 )
+
+// const cars = [ "BMW" , "Chevorlet", "Marcedes", "Volvo" ];
+
+// console.log( cars.join() )
+// console.log( cars.join(" ") )
+// console.log( cars.join(" -- ") )
+
+// let text = cars.join(" and ")
+// console.log( "the Text Value is : ", text )
+
+// const cars = [ "BMW" , "Chevorlet", "Marcedes", "Volvo" ];
+
+// for ( let i = 0; i < cars.length; i++ ){
+//     console.log( cars[i] )
+// }
+
+// for ( let i = 0; i < cars.length; i++ ){
+//     if ( cars[i] == 'Chevorlet'  ){
+//         continue;
+//     }
+//     console.log( cars[i] )
+// }
+
+let bmwx6 = {
+    "model" : "BMW X6 2020",
+    'color' : "Black",
+    'max-speed' : "360 Km/h"
+};
+
+console.log ( bmwx6['model'] )
+console.log ( bmwx6['color'] )
+console.log ( bmwx6['max-speed'] )
+console.log ( bmwx6[0] )
+console.log ( bmwx6.length )
