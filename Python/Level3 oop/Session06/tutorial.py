d871507e3814247dae06a09cf6f9b13b94bda170
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-12-30 16:58:45 +0200

    Task Python

---
+++ Python/Level3 oop/Session06/tutorial.py
@@ -0,0 +1,22 @@
+
+
+
+# -------------------------------------------------------
+# Task
+# Create a simple program to manage a student's basic details and display them using classes, attributes, and methods.
+# Step 1: Define the Class
+#     Create a class named Student.
+#     Add the following attributes:
+#         name (student's name)
+#         age (student's age)
+#         grade (student's grade)
+        
+# Step 2: Add a Method to Display Details
+#     Add a method named display_details() that prints the student's details.
+    
+# Step 3: Create a Student Object
+#     Create an instance of the Student class.
+#     Pass the name, age, and grade as arguments.
+
+# Step 4: Call the Method
+#     Use the display_details() method to print the student's information.
