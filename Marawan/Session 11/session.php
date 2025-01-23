b2f22c9f6c86fdcc2a75acc4032d44d7e9be8154
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2025-01-23 17:24:28 +0200

    Thrusday - 23/1/2025

---
+++ Marawan/Session 11/session.php
@@ -0,0 +1,53 @@
+
+<?php
+
+// setcookie('mode', 'dark-mode', time() + 15155545);
+// setcookie('mode', 'light-mode', time() + 15155545);
+// setcookie('mode', 'light-mode', time() - 10 );
+
+// session_start();
+
+// $_SESSION['userName'] = "Mohamed";
+
+// $_SESSION['error'] = "Email Cannot be Empty";
+
+// echo $_SESSION['userName'];
+
+// session_unset();
+// session_destroy();
+
+// echo "<pre>";
+// print_r($_SESSION);
+// echo "</pre>";
+
+// $value = "mohamed|'\"\"@yahoo.com";
+
+// var_dump(filter_var( $value , FILTER_VALIDATE_EMAIL));
+
+// echo "<br>";
+
+// echo date(' d m Y H:i:s ');
+
+// var_dump(filter_var( $value , FILTER_SANITIZE_EMAIL));
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="" method="post">
+        <input type="text" placeholder="Enter Name" name="user_name" >
+        <input type="email" placeholder="Enter Email" name="email">
+        <input type="password" placeholder="Enter Passowrd" name="pass">
+
+        <input type="submit">
+    </form>
+    
+</body>
+</html> -->
+
