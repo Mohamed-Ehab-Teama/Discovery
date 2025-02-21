14d9165f19fb3eca9c46aaf6a1680e786c14b2ff
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2025-02-21 17:53:25 +0200

    Friday - 21/2/2025

---
+++ GRC/Python/session01.py
@@ -0,0 +1,33 @@
+    # Juniors
+# 1
+# num = int( input("Enter Number Please: ") )
+# for i in range(1,11):
+#     print( f" {num} x {i} = {num*i} " )
+
+
+# 2 
+# grades = []
+# while True:
+#     name = input("Enter Name or 'done' to exit: ").lower()
+    
+#     if name == 'done' :
+#         print( sum(grades) / len(grades) )
+#         break
+    
+#     grades.append( int( input(f"Enter Grade for {name}: ") ) )
+
+
+# sum = 0
+# count = 0
+# while True:
+#     name = input("Enter Name or 'done' to exit ! ")
+    
+#     if name == 'done':
+#         print( sum / count )
+#         break
+    
+#     grade = int(input(f"Enter grade for {name} "))
+#     sum += grade
+#     count += 1
+    
+    
