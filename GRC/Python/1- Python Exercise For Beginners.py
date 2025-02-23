e9a28f9bdb3b577b8ab5b9dfe1e050ebcf963705
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2025-02-23 15:14:17 +0200

    preparation for GRC python

---
+++ GRC/Python/1- Python Exercise For Beginners.py
@@ -0,0 +1,162 @@
+# Exercise 1: Calculate the multiplication and sum of two numbers
+# Given two integer numbers, write a Python code to return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+# number1 = 20
+# number2 = 30
+# Expected Output: 600
+
+# number1 = 40
+# number2 = 30
+# Expected Output: 70
+
+# -----------------------------------------------------------------------
+
+# Exercise 2: Print the Sum of a Current Number and a Previous number
+# Write a Python code to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
+
+# -----------------------------------------------------------------------
+
+# Exercise 3: Print characters present at an even index number
+# Write a Python code to accept a string from the user and display characters present at an even index number.
+# For example, str = "PYnative". so your code should display ‘P’, ‘n’, ‘t’, ‘v’.
+
+# -----------------------------------------------------------------------
+
+# Exercise 4: Remove first n characters from a string
+# Write a Python code to remove characters from a string from 0 to n and return a new string.
+
+# For Example:
+# remove_chars("PYnative", 4) so output must be tive. Here, you need to remove the first four characters from a string.
+# remove_chars("PYnative", 2) so output must be native. Here, you need to remove the first two characters from a string.
+# Note: n must be less than the length of the string.
+
+# -----------------------------------------------------------------------
+
+# Exercise 5: Check if the first and last numbers of a list are the same
+# Write a code to return True if the list’s first and last numbers are the same. If the numbers are different, return False.
+
+# For Example:
+# Given list: [10, 20, 30, 40, 10]
+# result is True
+# numbers_y = [75, 65, 35, 75, 30]
+# result is False
+
+
+# -----------------------------------------------------------------------
+
+# Exercise 6: Display numbers divisible by 5
+# Write a Python code to display numbers from a list divisible by 5
+
+# Expected Output:
+# Given list is  [10, 20, 33, 46, 55]
+# Divisible by 5
+# 10
+# 20
+# 55
+
+# -----------------------------------------------------------------------
+
+# Exercise 7: Find the number of occurrences of a substring in a string
+# Write a Python code to find how often the substring “Emma” appears in the given string.
+
+# Given:
+# str_x = "Emma is good developer. Emma is a writer"
+# Expected Output:
+# Emma appeared 2 times
+
+# -----------------------------------------------------------------------
+
+# Exercise 8: Print the following pattern
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5
+
+# -----------------------------------------------------------------------
+
+# Exercise 9: Check Palindrome Number
+# Write a Python code to check if the given number is palindrome. A palindrome number is a number that is the same after reverse. For example, 545 is the palindrome number.
+
+# Expected Output:
+# original number 121
+# Yes. given number is palindrome number
+# original number 125
+# No. given number is not palindrome number
+
+# -----------------------------------------------------------------------
+
+# Exercise 10: Merge two lists using the following condition
+# Given two lists of numbers, write a Python code to create a new list such that the latest list should contain odd numbers from the first list and even numbers from the second list.
+
+# Given:
+# list1 = [10, 20, 25, 30, 35]
+# list2 = [40, 45, 60, 75, 90]
+# Expected Output:
+# result list: [25, 35, 40, 60, 90]
+
+# -----------------------------------------------------------------------
+
+# Exercise 11: Get each digit from a number in the reverse order.
+# For example, If the given integer number is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
+
+# -----------------------------------------------------------------------
+
+# Exercise 12: Calculate income tax
+# Calculate income tax for the given income by adhering to the rules below
+
+# Taxable Income	        Rate (in %)
+# First $10,000	            0
+# Next $10,000	            10
+# The remaining	            20
+
+# Expected Output:
+# For example, suppose the income is 45000, and the income tax payable is
+# 10000*0% + 10000*10%  + 25000*20% = $6000.
+
+# -----------------------------------------------------------------------
+
+# Exercise 13: Print multiplication table from 1 to 10
+
+# Expected Output:
+# 1  2 3 4 5 6 7 8 9 10 		
+# 2  4 6 8 10 12 14 16 18 20 		
+# 3  6 9 12 15 18 21 24 27 30 		
+# 4  8 12 16 20 24 28 32 36 40 		
+# 5  10 15 20 25 30 35 40 45 50 		
+# 6  12 18 24 30 36 42 48 54 60 		
+# 7  14 21 28 35 42 49 56 63 70 		
+# 8  16 24 32 40 48 56 64 72 80 		
+# 9  18 27 36 45 54 63 72 81 90 		
+# 10 20 30 40 50 60 70 80 90 100
+
+# -----------------------------------------------------------------------
+
+# Exercise 14: Print a downward half-pyramid pattern of stars
+# * * * * *  
+# * * * *  
+# * * *  
+# * *  
+# *
+
+# -----------------------------------------------------------------------
+
+# Exercise 15: Get an int value of base raises to the power of exponent
+# Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+
+# Note here exp is a non-negative integer, and the base is an integer.
+
+# Expected output
+
+# Case 1:
+# base = 2
+# exponent = 5
+# 2 raises to the power of 5: 32 i.e. (2 *2 * 2 *2 *2 = 32)
+
+# Case 2:
+# base = 5
+# exponent = 4
+# 5 raises to the power of 4 is: 625 
+# i.e. (5 *5 * 5 *5 = 625)
+
+# -----------------------------------------------------------------------
---
+++ GRC/Python/2- python input and output.py
@@ -0,0 +1,46 @@
+
+# Exercise 1: Accept numbers from a user
+# Write a program to accept two numbers from the user and calculate multiplication
+
+# -----------------------------------------------------------------------
+
+# Exercise 2: Display three string “Name”, “Is”, “James” as “Name**Is**James”
+# Use the print() function to format the given words in the mentioned format. Display the ** separator between each string.
+
+# Expected Output:
+# For example: print('Name', 'Is', 'James') will display Name**Is**James
+
+# -----------------------------------------------------------------------
+
+# Exercise 5: Accept a list of 5 float numbers as an input from the user
+
+# Take list as a input in Python.
+# Python list
+# Expected Output:
+# [78.6, 78.6, 85.3, 1.2, 3.5]
+
+# -----------------------------------------------------------------------
+
+# Exercise 7: Accept any three string from one input() call
+# Write a program to take three names as input from a user in the single input() function call.
+
+
+# Expected Output
+# Enter three string:  Emma Jessa Kelly
+# Name1: Emma
+# Name2: Jessa
+# Name3: Kelly
+
+# -----------------------------------------------------------------------
+
+# Exercise 8: Format variables using a string.format() method.
+# Write a program to use string.format() method to format the following three variables as per the expected output
+
+# Given:
+# totalMoney = 1000
+# quantity = 3
+# price = 450
+# Expected Output:
+# I have 1000 dollars so I can buy 3 football for 450.00 dollars.
+
+# -----------------------------------------------------------------------
