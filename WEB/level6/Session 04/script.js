47ce010442c15f6a08b6811bc458b9f1ca8b69ee
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2025-03-22 15:26:37 +0200

    Saturday - 22/3/2025

--- WEB/level6/Session 04/script.js
+++ WEB/level6/Session 04/script.js
@@ -1,1 +1,54 @@
 
+// localStorage.setItem('name', 'Mohamed')
+// localStorage.setItem('pass', '123456')
+// localStorage.setItem('age', '22')
+
+// let userName = localStorage.getItem('name')
+// let userPass = localStorage.getItem('pass')
+// let userAge = localStorage.getItem('age')
+// console.log(userName)
+// console.log(userPass)
+// console.log(userAge)
+
+
+// // -----------------------------------
+// console.log("========================================")
+// // -----------------------------------
+
+// sessionStorage.setItem('name', "Ali")
+// sessionStorage.setItem('age', "25")
+// sessionStorage.setItem('email', "ali@ali,com")
+
+// let sessionName = sessionStorage.getItem('name')
+// let sessionAge = sessionStorage.getItem('age')
+// let sessionEmail = sessionStorage.getItem('email')
+
+// console.log(sessionName)
+// console.log(sessionEmail)
+// console.log(sessionAge)
+
+// console.log("========================================")
+
+function ask()
+{
+    let my_name = prompt("Enter Your Name")
+    localStorage.setItem('name', my_name)
+
+    let my_age = prompt("Enter Your Age")
+    localStorage.setItem('age', my_age)
+
+    let my_email = prompt("Enter Your Email")
+    localStorage.setItem('email', my_email)
+
+}
+
+function show()
+{
+    my_name = localStorage.getItem('name')
+    my_age = localStorage.getItem('age')
+    my_email = localStorage.getItem('email')
+
+    alert(" Your name is " + my_name)
+    alert(" Your age is " + my_age)
+    alert(" Your email is " + my_email)
+}
---
+++ WEB/level6/Session 05/script.js
@@ -0,0 +1,37 @@
+// Initialize product list
+window.onload = displayProducts;
+
+// Create: Add product to local storage
+function addProduct() {
+    let productName = document.getElementById("productName").value;
+    if (productName) {
+        let products = JSON.parse(localStorage.getItem("products")) || [];
+        products.push(productName);
+        localStorage.setItem("products", JSON.stringify(products));
+        document.getElementById("productName").value = ""; // Clear input
+        displayProducts();
+    } else {
+        alert("Please enter a product name!");
+    }
+}
+
+// Display: Show all products in the list
+function displayProducts() {
+    let products = JSON.parse(localStorage.getItem("products")) || [];
+    let productList = document.getElementById("productList");
+    productList.innerHTML = ""; // Clear the list before re-rendering
+
+    products.forEach((product, index) => {
+        let li = document.createElement("li");
+        li.textContent = `${index + 1}. ${product}`;
+        productList.appendChild(li);
+    });
+}
+
+
+// Clear all products from Local storage
+function clearProducts()
+{
+    localStorage.clear()
+    displayProducts();
+}
