31c9cb992baa6be9ac5830fdfb12405822a07e6c
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2025-05-06 17:36:51 +0300

    Tuseday 6/5/2025

--- WEB/Level4 - JS/Session 03/script.js
+++ WEB/Level4 - JS/Session 03/script.js
@@ -1,28 +1,86 @@
-// for (i = 0; i <= 10; i++){
+
+
+// let v1 = 10
+// let v2 = "10"
+
+// if ( v1 == v2 ){
+//     console.log('Correct')
+// }else{
+//     console.log('Wrong')
+// }
+
+// if ( v1 === v2 ){
+//     console.log('Correct')
+// }else{
+//     console.log('Wrong')
+// }
+
+// =======================
+
+// Switch Statement
+
+// let day = "monday"
+
+// switch (day){
+//     case "saturday":
+//         console.log('Today is saturday')
+//         break;
+//     case "sunday":
+//         console.log('Today is sunday')
+//         break;
+//     case "monday":
+//         console.log('Today is monday')
+//         break;
+//     case "tuseday":
+//         console.log('Today is tuseday')
+//         break;
+//     case "wedensday":
+//         console.log('Today is wedensday')
+//         break;
+//     case "thrusday":
+//         console.log('Today is thrusday')
+//         break;
+//     case "friday":
+//         console.log('Today is friday')
+//         break;
+//     default:
+//         console.log('Wrong Day')
+// }
+
+// for ( let i = 0; i <= 10; i++ )
+// {
 //     console.log(i)
 // }
 
+// console.log('---------------------------')
 
-// for ( i = 10; i >= 0; i-- ){
-//     console.log(i)
+// for ( let j = 12; j >= 0; j-- )
+// {
+//     console.log(j)
+// }
+
+// console.log('---------------------------')
+
+// let num = 0
+// while ( num <= 20 ){
+//     console.log(num);
+//     num++;
+// }
+
+// 13 - 2
+
+
+// let num0 = 0
+
+// while (num0 >= 10)
+// {
+//     console.log(num0);
+//     num0++;
 // }
 
 
-// i = 0;
-// for( ; i <=12 ;  ){
-//     console.log(i)
-//     i += 2
-// }
-
-
-// x = 0
-// while ( x <= 20 ){
-//     console.log(x)
-//     x += 5
-// }
-
-x = 10
+let num10 = 0
 do {
-    console.log(x)
-    x--
-} while( x >= 20 )
+    console.log(num10);
+    num10++;
+} while( num10 >= 10 )
