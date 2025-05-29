2f6ab18af7157871f5af122c907b4a0ff88b172d
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2025-05-29 17:25:32 +0300

    Thrusday - 29/5/2025

---
+++ WEB/Level4 - JS Basic/Session 05/script.js
@@ -0,0 +1,53 @@
+// Function
+
+// function even_numbers() {
+//   for (i = 0; i <= 7; i++) {
+//     console.log(i);
+//   }
+// }
+
+// even_numbers();
+
+// console.log("==============================");
+
+// function add_two_numbers()
+// {
+//     console.log( 35 + 57 );
+// }
+
+// add_two_numbers();
+
+// console.log("==============================");
+
+// function add_two_numbers(z, v) {
+//   console.log(z + v);
+// }
+
+// add_two_numbers(30 , 50);
+// add_two_numbers(10 , 12);
+// add_two_numbers(150 , 365);
+
+// console.log("==============================");
+
+// function full_name( fname, lname )
+// {
+    // console.log( "Eng. " + fname + lname )
+    // console.log( "Eng. " , fname , lname )
+// }
+
+// full_name("Mohamed ", 'Ehab')
+
+// console.log("==============================");
+
+
+
+
+function calc( x , y )
+{
+    // console.log( x * y )
+    return x * y;
+}
+
+// console.log( calc( 15, 15 ) );
+
+calc(20, 20);
--- WEB/level5 - JS Advanced/session 03/script.js
+++ WEB/level5 - JS Advanced/session 03/script.js
@@ -1,6 +1,6 @@
 // let newPara = document.createElement('h1');
 // newPara.textContent = "This is New Heading";
-    
+
 // document.getElementById('container').appendChild(newPara);
 
 // let h2 = document.createElement('h2');
@@ -26,25 +26,71 @@
 //     alert('Nothing has happened')
 // }
 
+// function addPara() {
+//     let p1 = document.createElement('p');
+//     p1.textContent = " New Paragraph ";
+//     document.getElementById('container').appendChild(p1);
+// }
 
-function addPara() {
-    let p1 = document.createElement('p');
-    p1.textContent = " New Paragraph ";
-    document.getElementById('container').appendChild(p1);
-}
+// function removePara() {
+//     let parent = document.getElementById('container');
+//     parent.removeChild(parent.lastElementChild);
+// }
 
-function removePara() {
-    let parent = document.getElementById('container');
-    parent.removeChild(parent.lastElementChild);
-}
+// let btn = document.getElementById('btn');
 
-let btn = document.getElementById('btn');
+// btn.onmouseover = function() {
+//     alert("On Mouse Over")
+// }
 
-btn.onmouseover = function() {
-    alert("On Mouse Over")
-}
+// btn.onmouseleave = function() {
+//     alert("On Mouse Leave")
+// }
 
-btn.onmouseleave = function() {
-    alert("On Mouse Leave")
-}
+// -------------------------------------------------------
 
+// let box01 = document.createElement('div');
+// box01.innerText = " This Div is created usong JS ";
+// box01.classList = "Hanin test";
+// box01.style.color = 'red';
+
+// let body = document.getElementById('body');
+// body.appendChild(box01);
+
+// box01.remove();
+
+// alert( 'Hello JS' );
+
+// let userName = prompt("Enter Your Name");
+
+// alert( `Hello ${userName}` );
+
+// confirm('Are You Okay ?')
+
+// if ( confirm( 'Are You Good???' ) )
+// {
+//     alert('Fine')
+// }else{
+//     alert('Waht a bad luck!!')
+// }
+
+// =>Welcome  => What's Your Name? => How old are you?
+// Confirm -> Do You to try again?
+
+// function wel(){
+//     if ( confirm('do you wanna try again ?') )
+//     {
+//         wel()
+//     }else{
+//         return 0;
+//     }
+// }
+
+// -----------------------------------------
+
+
+// function say_welcome()
+// {
+//     var userName = prompt('Enter Your Name')
+//     alert(`Hello ${userName}`)
+// }
